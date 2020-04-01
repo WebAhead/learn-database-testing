21befe3e32c12b659b7f66918b14a01c6aa9bada
oliverjam <oliverjamesphillips@gmail.com>
2020-04-01 14:01:40 +0100

    Add final test solution

--- solution/tests/model.test.js
+++ solution/tests/model.test.js
@@ -1,24 +1,26 @@
 const test = require("tape");
-const db = require("../database/connection");
 const build = require("../database/build");
 const { getUsers, createUser, getPosts } = require("../model");
 
 test("Can get all users", t => {
-  build().then(() => {
-    getUsers()
-      .then(users => {
-        const firstUser = users[0];
-        t.equal(firstUser.username, "Sery1976");
-        t.equal(firstUser.age, 28);
-      })
-      .catch(error => {
-        t.error(error);
-      })
-      .finally(() => {
-        t.end();
-        // db.end();
-      });
-  });
+  build()
+    .then(() => {
+      getUsers()
+        .then(users => {
+          const firstUser = users[0];
+          t.equal(firstUser.username, "Sery1976");
+          t.equal(firstUser.age, 28);
+          t.end();
+        })
+        .catch(error => {
+          t.error(error);
+          t.end();
+        });
+    })
+    .catch(buildError => {
+      t.error(buildError);
+      t.end();
+    });
 });
 
 test("Can create a new user", t => {
@@ -29,6 +31,25 @@ test("Can create a new user", t => {
       .then(users => {
         const latestUser = users[users.length - 1];
         t.equal(latestUser.username, "oli");
+        t.end();
+      })
+      .catch(error => {
+        t.error(error);
+        t.end();
+      });
+  });
+});
+
+test("Can get posts with authors", t => {
+  build().then(() => {
+    getPosts()
+      .then(posts => {
+        const firstPost = posts[0];
+        t.equal(firstPost.username, "Sery1976");
+        t.equal(
+          firstPost.text_content,
+          "Announcing of invitation principles in."
+        );
         t.end();
       })
       .catch(error => {
