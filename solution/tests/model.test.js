f6201c6509654ad1f8dd15c453c35ce91385d91f
Oliver Phillips <oliverjamesphillips@gmail.com>
2020-08-09 21:27:36 +0100

    Close DB pool after tests run

--- solution/tests/model.test.js
+++ solution/tests/model.test.js
@@ -1,49 +1,50 @@
 const test = require("tape");
 const build = require("../database/build");
 const { getUsers, createUser, getPosts } = require("../model");
+const db = require("../database/connection");
 
-test("Can get all users", t => {
+test("Can get all users", (t) => {
   build()
     .then(() => {
       getUsers()
-        .then(users => {
+        .then((users) => {
           const firstUser = users[0];
           t.equal(firstUser.username, "Sery1976");
           t.equal(firstUser.age, 28);
           t.end();
         })
-        .catch(error => {
+        .catch((error) => {
           t.error(error);
           t.end();
         });
     })
-    .catch(buildError => {
+    .catch((buildError) => {
       t.error(buildError);
       t.end();
     });
 });
 
-test("Can create a new user", t => {
+test("Can create a new user", (t) => {
   build().then(() => {
     const data = { username: "oli", age: 29, location: "London" };
     createUser(data)
       .then(getUsers)
-      .then(users => {
+      .then((users) => {
         const latestUser = users[users.length - 1];
         t.equal(latestUser.username, "oli");
         t.end();
       })
-      .catch(error => {
+      .catch((error) => {
         t.error(error);
         t.end();
       });
   });
 });
 
-test("Can get posts with authors", t => {
+test("Can get posts with authors", (t) => {
   build().then(() => {
     getPosts()
-      .then(posts => {
+      .then((posts) => {
         const firstPost = posts[0];
         t.equal(firstPost.username, "Sery1976");
         t.equal(
@@ -52,9 +53,15 @@ test("Can get posts with authors", t => {
         );
         t.end();
       })
-      .catch(error => {
+      .catch((error) => {
         t.error(error);
         t.end();
       });
   });
+});
+
+test("Close DB pool (not a real test)", (t) => {
+  // otherwise tests will pause for 10s in the terminal
+  db.end();
+  t.end();
 });
