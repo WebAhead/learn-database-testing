728c6adcfb636727c6a8623da1633315c2b94c90
Oliver Phillips <oliverjamesphillips@gmail.com>
2020-08-09 21:27:20 +0100

    Allow build script to be run directly

--- solution/database/build.js
+++ solution/database/build.js
@@ -10,4 +10,12 @@ function build() {
   return db.query(initSQL);
 }
 
+// this will only run if this file is executed directly
+// e.g. run `node solution/database/build.js` in your terminal
+// https://nodejs.org/api/modules.html#modules_accessing_the_main_module
+// this allows us to use this to rebuild our DB easily
+if (require.main === module) {
+  build().then(() => db.end());
+}
+
 module.exports = build;
