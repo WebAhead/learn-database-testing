742ecf5f5085930b1e6485a6c229c78be56cb4b7
Oliver Phillips <oliverjamesphillips@gmail.com>
2020-08-09 21:27:03 +0100

    Update DB connection to use connectionString

--- workshop/database/connection.js
+++ workshop/database/connection.js
@@ -1,8 +1,17 @@
 const pg = require("pg");
 const dotenv = require("dotenv");
 
-dotenv.config(); // load environment variables
+// load environment variables from the ".env" file
+dotenv.config();
 
-const db = new pg.Pool(); // create a pool of available connections
+// grab the URL for our local database
+const connectionString = process.env.DATABASE_URL;
 
-module.exports = db; // export the pool for use elsewhere on our server
+// create a pool of available connections
+// we can use this to query our database
+const db = new pg.Pool({
+  connectionString,
+});
+
+// export the pool for use elsewhere on our server
+module.exports = db;
